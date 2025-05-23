558ff3a94847d064f40f45038aa258bf4e82fe9c
Vlad Choporov <Vladachi8@gmail.com>
2025-05-23 22:56:02 +0300

    task_2

--- Homeworks/src/java/lesson2_3/Product.java
+++ Homeworks/src/java/lesson2_3/Product.java
@@ -10,7 +10,7 @@ public class Product {
     protected int price;
     protected Boolean isBooked;
 
-    public Product (String name, LocalDate productionDate, String fabricator, String originCountry, int price, Boolean booking) {
+    public Product(String name, LocalDate productionDate, String fabricator, String originCountry, int price, Boolean booking) {
         this.name = name;
         this.productionDate = productionDate;
         this.fabricator = fabricator;
@@ -31,7 +31,7 @@ public class Product {
     public static void main(String[] args) {
         Product product1 = new Product(
                 "Телевизор",
-                LocalDate.of(2022,5,15),
+                LocalDate.of(2022, 5, 15),
                 "Firma bla bla",
                 "USA",
                 288,
@@ -39,5 +39,67 @@ public class Product {
         );
 
         product1.showInf();
+
+        //2 задание
+        Product[] productsArray = new Product[5];
+
+        productsArray[0] = new Product(
+                "Samsung S25 Ultra",
+                LocalDate.of(2025, 5, 31),
+                "Samsung Corp.",
+                "Korea",
+                5599,
+                true
+        );
+
+        productsArray[1] = new Product(
+                "Samsung S24 Ultra",
+                LocalDate.of(2020, 1, 1),
+                "Samsung Corp.",
+                "Korea",
+                4599,
+                false
+        );
+
+        productsArray[2] = new Product(
+                "Samsung 65",
+                LocalDate.of(2023, 6, 7),
+                "Samsung Corp.",
+                "Korea",
+                7599,
+                false
+        );
+
+        productsArray[3] = new Product(
+                "LG 80",
+                LocalDate.of(2025, 12, 31),
+                "LG Electronics",
+                "USA",
+                18000,
+                true
+        );
+
+        productsArray[4] = new Product(
+                "LG 55",
+                LocalDate.of(2022, 7, 15),
+                "LG Electronics",
+                "USA",
+                9000,
+                true
+        );
+
+        printProductsArray(productsArray);
+    }
+
+    // Метод для вывода массива товаров
+    public static void printProductsArray(Product[] productsArray) {
+        System.out.println();
+        System.out.println("Задание 2 -> Массив товаров:");
+
+        for (int i = 0; i < productsArray.length; i++) {
+            System.out.println();
+            productsArray[i].showInf();
+        }
     }
 }
+
