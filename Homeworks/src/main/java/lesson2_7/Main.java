d207c243c0af6f5ec577efbcdd0d71e4e371a0cc
Vlad Choporov <Vladachi8@gmail.com>
2025-06-08 17:28:51 +0300

    add classes for testing, add junit 5

---
+++ Homeworks/src/main/java/lesson2_7/Main.java
@@ -0,0 +1,15 @@
+package lesson2_7;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Факториал 10: " + Factorial.factorial(-1));
+        System.out.println("Площадь треугольника: " + TriangleArea.calculateArea(6, 3));
+
+        System.out.println("4 + 6 = " + ArithmeticOperations.add(4, 6));
+        System.out.println("330 - 30 = " + ArithmeticOperations.subtract(330, 30));
+        System.out.println("2 * 100 = " + ArithmeticOperations.multiply(2, 100));
+        System.out.println("21 / 3 = " + ArithmeticOperations.divide(21, 3));
+
+        System.out.println(NumberComparator.compare(500, 2));
+    }
+}
---
+++ Homeworks/src/test/java/lesson2_7/FactorialTest.java
@@ -0,0 +1,28 @@
+package lesson2_7;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.*;
+
+class FactorialTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "0, 1",
+            "1, 1",
+            "4, 24",
+            "10, 3628800"
+    })
+    void testFactorialValidInput(int input, long expected) {
+        assertEquals(expected, Factorial.factorial(input));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -7, -200})
+    void testFactorialInvalidInput(int input) {
+        assertThrows(IllegalArgumentException.class,
+                () -> Factorial.factorial(input));
+    }
+}
