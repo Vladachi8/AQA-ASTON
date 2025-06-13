68daae16de9567feb7cef77b712c91b05b5cbdd0
Vlad Choporov <Vladachi8@gmail.com>
2025-06-13 21:15:50 +0300

    fix PostFormData class

--- Homeworks/src/test/java/TestPostmanMethods/PostFormData.java
+++ Homeworks/src/test/java/TestPostmanMethods/PostFormData.java
@@ -6,22 +6,26 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class PostFormData {
     private static final String BASE_URL = "https://postman-echo.com";
+    private static final String REQUEST_BODY = "{\"foo1\":\"bar1\",\"foo2\":\"bar2\"}";
 
     @Test
     public void testPostFormData() {
         given()
+                .log()
+                .body()
                 .baseUri(BASE_URL)
-                .contentType("application/x-www-form-urlencoded")
-                .formParam("foo1", "bar1")
-                .formParam("foo2", "bar2")
+                .contentType("application/json")
+                .body(REQUEST_BODY)
 
                 .when()
                 .post("/post")
 
                 .then()
+                .log()
+                .body()
                 .statusCode(200)
                 .contentType("application/JSON")
-                .body("form.foo1", equalTo("bar1"))
-                .body("form.foo2", equalTo("bar2"));
+                .body("data.foo1", equalTo("bar1"))
+                .body("data.foo2", equalTo("bar2"));
     }
 }
